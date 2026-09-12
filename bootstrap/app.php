b3d7f34ce71c27086d673efcbd73ba776ad804af
Reyhan <reyhanrainaldy.rr@gmail.com>
2026-09-12 13:07:57 +0700

    Configure app useStoragePath before returning app instance for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+$storagePath = env('APP_STORAGE', '/tmp/storage');
+@mkdir($storagePath . '/framework/views', 0755, true);
+@mkdir($storagePath . '/framework/cache/data', 0755, true);
+@mkdir($storagePath . '/framework/sessions', 0755, true);
+@mkdir($storagePath . '/logs', 0755, true);
+
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,13 +22,9 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
 
-$storagePath = env('APP_STORAGE', '/tmp/storage');
-@mkdir($storagePath . '/framework/views', 0755, true);
-@mkdir($storagePath . '/framework/cache/data', 0755, true);
-@mkdir($storagePath . '/framework/sessions', 0755, true);
-@mkdir($storagePath . '/logs', 0755, true);
 $app->useStoragePath($storagePath);
 
 return $app;
